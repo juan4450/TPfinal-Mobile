be2378ed5eb4a04830a54b315cd85122c85f3557
lujanmaccari <mlujanmaccari@gmail.com>
2025-06-29 15:23:18 -0300

    ajustes de diseño para seccion "Mi perfil"

--- app/src/main/java/com/example/tpfinalmobile/PerfilActivity.kt
+++ app/src/main/java/com/example/tpfinalmobile/PerfilActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -68,6 +69,11 @@ class PerfilActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         tvMaterias.text = getString(R.string.profile_subjects_label) + " 13/23"
         tvPromedio.text = getString(R.string.profile_average_label) + " 8.7"
         tvEstado.text = getString(R.string.profile_academicStatus_label) + " Regular"
+
+        val btnVolver: ImageView = findViewById(R.id.btnVolver)
+        btnVolver.setOnClickListener {
+            finish()
+        }
     }
 
     override fun onBackPressed() {
